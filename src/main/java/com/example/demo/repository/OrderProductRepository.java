653f74e0dacc925ac9b93a6eab44e9051f78dae5
sarawaszpan <pracainzwsb2018@gmail.com>
2019-10-07 09:27:33 +0200

    Change frontend directory

--- src/main/java/com/example/demo/repository/OrderProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.OrderProduct;
-import com.example.demo.model.OrderProductPK;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderProductRepository extends CrudRepository<OrderProduct, OrderProductPK> {
-}
--- src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
