fa46c033eb89cd42c167fe4a73fea1f0c29bdfab
Livio <hipzap@gmail.com>
2019-10-07 12:01:11 +0200

    Build backend in front End Services etc

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import{ProductsComponent} from "./frontend/components/products/products.component";
+import{ShoppingCartComponent} from "./frontend/components/shopping-cart/shopping-cart.component";
+import{OrdersComponent} from "./frontend/components/orders/orders.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'frontend';
+  private collapsed = true;
+  orderFinished = false;
+
+  @ViewChild('productsC')
+  productsC: ProductsComponent;
+
+  @ViewChild('shoppingCartC')
+  shoppingCartC: ShoppingCartComponent;
+
+  @ViewChild('ordersC')
+  ordersC: OrdersComponent;
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  finishOrder(orderFinished: boolean) {
+    this.orderFinished = orderFinished;
+  }
+
+  reset() {
+    this.orderFinished = false;
+    this.productsC.reset();
+    this.shoppingCartC.reset();
+    this.ordersC.paid = false;
+  }
 }
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './frontend/components/nav-bar/nav-bar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './frontend/components/products/products.component';
+import { OrdersComponent } from './frontend/components/orders/orders.component';
+import { ShoppingCartComponent } from './frontend/components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProductsComponent,
+    OrdersComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
@@ -18,4 +24,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
